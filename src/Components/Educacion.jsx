1713ab7cf4571b12c8e03a67165d810d5e592e19
marcosali <108292814+marcosali@users.noreply.github.com>
2024-04-25 11:55:30 -0500

    Update Educacion.jsx

--- src/Components/Educacion.jsx
+++ src/Components/Educacion.jsx
@@ -40,11 +40,11 @@ const Educacion = () => {
 
                 {/* barra de progreso */}
                 <ProgressBar skill="HTML/CSS" percent={90} /><br />
+                <ProgressBar skill="Tailwind" percent={60} /><br />
                 <ProgressBar skill="JAVASCRIPT" percent={50} /><br />
                 <ProgressBar skill="REACT" percent={60} /><br />
                 <ProgressBar skill="PHP" percent={45} /><br />
-                <ProgressBar skill="SQL" percent={30} /><br />
-                <ProgressBar skill="Tailwind" percent={50} />
+                <ProgressBar skill="SQL" percent={30} />
             </div>
         </div>
     );
