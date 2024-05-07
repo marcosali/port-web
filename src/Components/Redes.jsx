ff28d37024499d4df82987febaaaa2f4175b5968
marcosali <108292814+marcosali@users.noreply.github.com>
2024-05-07 16:08:40 -0500

    Update Redes.jsx

--- src/Components/Redes.jsx
+++ src/Components/Redes.jsx
@@ -4,9 +4,7 @@ import './Redes.css';
 const Redes = () => {
     return (
         <footer className="redes-container">
-            <div className="footer-content">
-                <p>© 2024 Tu Empresa. Todos los derechos reservados.</p>
-            </div>
+       
         </footer>
     );
 }
