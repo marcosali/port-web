5df26c73739d98af0bd7f438f2ea5ad446a82dd5
marcosali <108292814+marcosali@users.noreply.github.com>
2024-05-10 15:46:23 -0500

    Update Redes.jsx

--- src/Components/Redes.jsx
+++ src/Components/Redes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './Redes.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTiktok, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Redes = () => {
     useEffect(() => {
@@ -17,9 +18,14 @@ const Redes = () => {
     return (
         <footer className="redes-container">
             <div className="footer-content">
-                <a href="#inicio" id="btn-scroll-up"><FontAwesomeIcon className='bg-yellow-400 w-10 h-10 rounded-full' icon={faArrowUp} /></a>
+                <a href="#inicio" id="btn-scroll-up"><FontAwesomeIcon className='flecha bg-yellow-400 w-10 h-10 rounded-full' icon={faArrowUp} /></a>
+                <br /><br />
+                <a href="https://www.facebook.com/profile.php?id=100008948221444&mibextid=ZbWKwL"><FontAwesomeIcon className='bg-blue-500 w-10 h-10 rounded-full' icon={faFacebook} /></a>
+                <a href="/"><FontAwesomeIcon className='bg-red-500 w-10 h-10 rounded-full' icon={faTiktok} /></a>
+                <a href="/"><FontAwesomeIcon className='bg-purple-500 w-10 h-10 rounded-full' icon={faInstagram} /></a>
+                <a href="/"><FontAwesomeIcon className='bg-blue-400 text-white w-10 h-10 rounded-full p-2' icon={faTwitter} /></a>
+                <p> Marco Salinas © 2024</p>
             </div>
-            
         </footer>
     );
 }
