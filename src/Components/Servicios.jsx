3720df76469851f6ac15780659afd9c80144579f
marcosali <108292814+marcosali@users.noreply.github.com>
2024-05-10 16:12:35 -0500

    Update Servicios.jsx

--- src/Components/Servicios.jsx
+++ src/Components/Servicios.jsx
@@ -10,13 +10,13 @@ const Servicios = () => {
             <div className="contenido">
             <p><span>&mdash;&mdash;&mdash; Desarrollo web</span></p>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-world-www"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 7a9 9 0 0 0 -7.5 -4a8.991 8.991 0 0 0 -7.484 4" /><path d="M11.5 3a16.989 16.989 0 0 0 -1.826 4" /><path d="M12.5 3a16.989 16.989 0 0 1 1.828 4" /><path d="M19.5 17a9 9 0 0 1 -7.5 4a8.991 8.991 0 0 1 -7.484 -4" /><path d="M11.5 21a16.989 16.989 0 0 1 -1.826 -4" /><path d="M12.5 21a16.989 16.989 0 0 0 1.828 -4" /><path d="M2 10l1 4l1.5 -4l1.5 4l1 -4" /><path d="M17 10l1 4l1.5 -4l1.5 4l1 -4" /><path d="M9.5 10l1 4l1.5 -4l1.5 4l1 -4" /></svg>
-            <p>Crea y mantén tus sitios web y también cuida su rendimiento y capacidad de tráfico</p>
+            <p><spa className="mora">Crea y mantén</spa> tus sitios web y también cuida su rendimiento y capacidad de tráfico</p>
             <p><span>&mdash;&mdash;&mdash; Desarrollo de aplicaciones</span></p>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-html5"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" /><path d="M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" /></svg>
-            <p>Diseño, construcción e implementación estándar de sus aplicaciones con documentación.</p>
+            <p><spa className="mora">Diseño, construcción e implementación</spa> estándar de sus aplicaciones con documentación.</p>
             <p><span>&mdash;&mdash;&mdash; Creatividad</span></p>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bulb"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7" /><path d="M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3" /><path d="M9.7 17l4.6 0" /></svg>
-            <p>Convierte lo que tienes en mente de un producto digital en realidad. Para cualquier plataforma que consideres.</p>
+            <p><spa className="mora">Convierte</spa> lo que tienes en mente de un producto digital en realidad. Para cualquier plataforma que consideres.</p>
             </div>
         </div>
     )
